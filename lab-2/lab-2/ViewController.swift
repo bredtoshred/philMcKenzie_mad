0194f8386cb8bfa5f30e68635f8f863229334fe0
Phil <phillip.mckenzie@colorado.edu>
2017-09-14 12:39:32 -0600

    initial upload of lab 2, all elements added, no functionality yet

---
+++ lab-2/lab-2/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  lab-2
+//
+//  Created by Phillip James Mckenzie on 9/14/17.
+//  Copyright © 2017 Phillip James Mckenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageLocation: UIImageView!
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var imageSelector: UISegmentedControl!
+    @IBOutlet weak var fontColorSwitch: UISwitch!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
