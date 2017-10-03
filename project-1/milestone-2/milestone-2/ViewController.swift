45eab344506372404d214af6c9f38047062a5501
Phil <phillip.mckenzie@colorado.edu>
2017-10-03 12:49:27 -0600

    layout complete and tested for all portrait devices, still needs funtionality

---
+++ project-1/milestone-2/milestone-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  milestone-2
+//
+//  Created by Phil McKenzie on 10/3/17.
+//  Copyright © 2017 Phil McKenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
